24d75c2ab8712a6767f7dc6074c093a929e45370
Brondchux <brondchux@gmail.com>
2018-11-25 23:03:06 -0500

    First test on localhost:2000

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var expressHbs = require('express-handlebars');
 var passport = require('passport');
 var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
 // all route pages must be declared and assigned to variables here
 var indexRouter = require('./routes/index');
-var userRouter = require('./routes/user/user');
 
+// initiate the express engine and store in variable app
 var app = express();
 
+// establish a mongo database connection
 mongoose.connect("mongodb://localhost:27017/itemz");
 
 // pull in passport configuration to enable user login/logout
 require('./config/passport');
+
+// view engine setup
+app.engine('.hbs', expressHbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(validator());
+app.use(cookieParser());
+app.use(session({
+	secret: 'mygiantsecret', 
+	resave: false, 
+	saveUnintialized: false,
+	store: new MongoStore({mongooseConnection: mongoose.connection}),
+	cookie: { maxAge: 180 * 60 * 1000 }
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -37,7 +53,6 @@ app.use(function(req, res, next){
 });
 
 // all route variables are used here to tie to a route url /example
-app.use('/user', userRouter);
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
@@ -54,6 +69,13 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Set Port
+app.set('port', (process.env.PORT || 2000));
+
+app.listen(app.get('port'), function(){
+	console.log('Itemz server started at port ' + app.get('port'));
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,8 @@ router.use(csrfProtection);
 /* Website default pages and links to other pages */
 router.get('/', function(req, res, next) {
 	// redirect to home page
-	res.render('web/index', {title: "Gospels Foundation", layout: 'layout.hbs', breadcrumb: true});
-});
-
-router.get('/signup', User.notLoggedIn, function(req, res, next) {
-	var messages = req.flash('error');
-	// redirect to signin page
-	res.render('web/signup', { title: 'Sign Up', layout: 'layout.hbs', csrfToken: req.csrfToken, messages: messages, hasErrors: messages.length > 0 });
-});
-
-router.get('/signin', User.notLoggedIn, function(req, res, next) {
-	var messages = req.flash('error');
-	// redirect to signup page
-	res.render('web/signin', { title: 'Sign In', layout: 'layout.hbs', csrfToken: req.csrfToken, messages: messages, hasErrors: messages.length > 0 });
+	let output = 'Itemz API - Exchange of Itemz';
+	res.json(output);
 });
 
 ////////////// POSTS /////////////
