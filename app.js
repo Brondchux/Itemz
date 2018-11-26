a9fb4f550ebdb459aa0ca69e6e45cdc133f1b7c9
Brondchux <brondchux@gmail.com>
2018-11-25 21:30:48 -0500

    Authentication Dev Stage

---
+++ app.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
+var validator = require('express-validator');
